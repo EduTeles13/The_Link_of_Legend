6f0bfc6a8034a13499538b70aa7804ce2a65e669
Eduardo Teles <eduardodiasteles13@gmail.com>
2022-05-04 13:27:26 -0300

    jogo base final

--- objetos.py
+++ objetos.py
@@ -8,7 +8,7 @@ class Objetos(pygame.sprite.Sprite):
         self.image = pygame.image.load('data/crystal blue.png')
         self.image = pygame.transform.scale(self.image, [30, 30])
         self.rect = pygame.Rect(50, 50, 30, 30)
-        self.rect.x = random.randint(1,1000)
+        self.rect.x = random.randint(10,1000)
         self.rect.y = random.randint(50,700)
 
 class Objetos1(pygame.sprite.Sprite):
@@ -18,7 +18,7 @@ class Objetos1(pygame.sprite.Sprite):
         self.image = pygame.image.load('data/crystal green.png')
         self.image = pygame.transform.scale(self.image, [30, 30])
         self.rect = pygame.Rect(100, 100, 30, 30)
-        self.rect.x = random.randint(1, 1000)
+        self.rect.x = random.randint(10, 1000)
         self.rect.y = random.randint(50,700)
 
 class Objetos2(pygame.sprite.Sprite):
@@ -28,5 +28,5 @@ class Objetos2(pygame.sprite.Sprite):
         self.image = pygame.image.load('data/crystal red.png')
         self.image = pygame.transform.scale(self.image, [30, 30])
         self.rect = pygame.Rect(200, 200, 30, 30)
-        self.rect.x = random.randint(1, 1000)
+        self.rect.x = random.randint(10, 1000)
         self.rect.y = random.randint(50,700)
