db0ad4f881f66bdce504173d4ffc632b6f8385da
ncdrneto <ncrneto@gmail.com>
2022-05-09 10:28:35 -0300

    enviando de fato o código

--- jogobase.py
+++ jogobase.py
@@ -5,23 +5,32 @@ from objetos import Objetos1
 from objetos import Objetos2
 import random
 
-#inicializando o pygame e criando a janela
+def draw_rect_alpha(surface, color, rect):
+    shape_surf = pygame.Surface(pygame.Rect(rect).size, pygame.SRCALPHA)
+    pygame.draw.rect(shape_surf, color, shape_surf.get_rect())
+    surface.blit(shape_surf, rect)
+
+# inicializando o pygame e criando a janela
 pygame.init()
 altura = 720
 largura = 1080
-display = pygame.display.set_mode([largura,altura])
+display = pygame.display.set_mode([largura, altura])
 pygame.display.set_caption('Garantir a Nota')
+bg = pygame.image.load("data/Mapa_1.png")
 
-#objects
+# objects
 object_group = pygame.sprite.Group()
 objetos_group = pygame.sprite.Group()
 objetos_group1 = pygame.sprite.Group()
 objetos_group2 = pygame.sprite.Group()
 
+# obstaculos
+obstaculo_1 = pygame.Rect(0, 374, 308, 50)
+
 player = Player(object_group)
-spawn = random.randint(5,8)
-spawn1 = random.randint(5,8)
-spawn2 = random.randint(5,8)
+spawn = random.randint(5, 8)
+spawn1 = random.randint(5, 8)
+spawn2 = random.randint(5, 8)
 for i in range(spawn):
     novo_objeto = Objetos(object_group, objetos_group)
 for i in range(spawn1):
@@ -38,9 +47,9 @@ contagem_rubi = 0
 # tela
 fonte = pygame.font.Font(None, 35)
 fim_de_jogo = pygame.font.Font(None, 60)
-texto = fonte.render("esmeralda:"+str(contagem_esmeralda),True,(255, 255, 255), (0, 0, 0))
-texto1 = fonte.render("diamante:"+str(contagem_rubi),True,(255, 255, 255), (0, 0, 0))
-texto2 = fonte.render("rubi:"+str(contagem_cristal),True,(255, 255, 255), (0, 0, 0))
+texto = fonte.render("esmeralda:"+str(contagem_esmeralda), True, (255, 255, 255), (0, 0, 0))
+texto1 = fonte.render("diamante:"+str(contagem_rubi), True, (255, 255, 255), (0, 0, 0))
+texto2 = fonte.render("rubi:"+str(contagem_cristal), True, (255, 255, 255), (0, 0, 0))
 FIMDEJOGO = fim_de_jogo.render("parabéns você garantiu o projeto de P1!!!", True, (255, 255, 255), (0, 0, 0))
 
 gameover = False
@@ -49,6 +58,8 @@ if __name__ == '__main__':
     while gameLoop:
             object_group.update()
             clock.tick(60)
+            display.blit(bg, (0, 0))
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     gameLoop = False
@@ -57,26 +68,24 @@ if __name__ == '__main__':
                 collisions1 = pygame.sprite.spritecollide(player, objetos_group1, True, pygame.sprite.collide_mask)
                 collisions2 = pygame.sprite.spritecollide(player, objetos_group2, True, pygame.sprite.collide_mask)
 
-                #contagem dos objetos
+                # contagem dos objetos
                 if collisions:
-                    contagem_cristal +=1
+                    contagem_cristal += 1
                     texto1 = fonte.render("diamante:" + str(contagem_cristal), True, (255, 255, 255), (0, 0, 0))
                 elif collisions1:
-                    contagem_esmeralda+=1
+                    contagem_esmeralda += 1
                     texto = fonte.render("esmeralda:" + str(contagem_esmeralda), True, (255, 255, 255), (0, 0, 0))
                 elif collisions2:
-                    contagem_rubi +=1
+                    contagem_rubi += 1
                     texto2 = fonte.render("rubi:" + str(contagem_rubi), True, (255, 255, 255), (0, 0, 0))
 
                 if contagem_cristal == spawn and contagem_esmeralda == spawn1 and contagem_rubi == spawn2:
                     gameover = True
 
-
-                display.fill([0,0,0])
                 display.blit(texto, [0, 0])
                 display.blit(texto1, [400, 0])
                 display.blit(texto2, [900, 0])
-                if gameover == True:
+                if gameover is True:
                     display.blit(FIMDEJOGO, [10, 140])
 
                 object_group.update()
--- objetos.py
+++ objetos.py
@@ -8,8 +8,8 @@ class Objetos(pygame.sprite.Sprite):
         self.image = pygame.image.load('data/crystal blue.png')
         self.image = pygame.transform.scale(self.image, [30, 30])
         self.rect = pygame.Rect(50, 50, 30, 30)
-        self.rect.x = random.randint(10,1000)
-        self.rect.y = random.randint(50,700)
+        self.rect.x = random.randint(10, 1000)
+        self.rect.y = random.randint(50, 700)
 
 class Objetos1(pygame.sprite.Sprite):
     def __init__(self, *groups):
@@ -19,7 +19,7 @@ class Objetos1(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.image, [30, 30])
         self.rect = pygame.Rect(100, 100, 30, 30)
         self.rect.x = random.randint(10, 1000)
-        self.rect.y = random.randint(50,700)
+        self.rect.y = random.randint(50, 700)
 
 class Objetos2(pygame.sprite.Sprite):
     def __init__(self, *groups):
@@ -29,4 +29,4 @@ class Objetos2(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.image, [30, 30])
         self.rect = pygame.Rect(200, 200, 30, 30)
         self.rect.x = random.randint(10, 1000)
-        self.rect.y = random.randint(50,700)
+        self.rect.y = random.randint(50, 700)
